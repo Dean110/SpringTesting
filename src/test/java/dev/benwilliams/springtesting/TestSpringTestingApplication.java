e73d2c2d7cc266c30705b24353fda0a268951dd3
Ben Williams <ben@benwilliams.dev>
2025-11-10 18:21:05 -0500

    fix: Changing project setup to a running state

--- src/test/java/dev/benwilliams/springtesting/TestSpringTestingApplication.java
+++ src/test/java/dev/benwilliams/springtesting/TestSpringTestingApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class TestSpringTestingApplication {
 
     public static void main(String[] args) {
-        SpringApplication.from(SpringTestingApplication::main).with(TestcontainersConfiguration.class).run(args);
+        SpringApplication.from(SpringTestingApplication::main).run(args);
     }
 
 }
